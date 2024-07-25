7714c08a89149fde219e8ec6b672982693fa71a0
King-George-Hughes <hughesgeorgeking@gmail.com>
2024-07-25 09:02:32 +0000

    more changes

--- app/(public)/booking/page.tsx
+++ app/(public)/booking/page.tsx
@@ -1,20 +1,7 @@
 "use client";
 
-import axiosClient from "@/services/axios-client";
-import React, { useState } from "react";
-
 const BookingPage = () => {
-  const [data, setData] = useState();
-
-  const allStocks = async () =>
-    await axiosClient
-      .get("/stock")
-      .then(({ data }) => console.log(data))
-      .catch((error) => console.log(error));
-
-  allStocks();
-
-  return <div>BookingPage</div>;
+  <div>Booking page</div>;
 };
 
 export default BookingPage;
