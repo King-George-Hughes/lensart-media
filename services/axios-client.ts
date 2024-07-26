de4753f9ebe0b565989c7b852bcf021824008303
King-George-Hughes <hughesgeorgeking@gmail.com>
2024-07-26 08:43:34 +0000

    more changes

--- services/axios-client.ts
+++ services/axios-client.ts
@@ -6,33 +6,3 @@ const axiosInstance = axios.create({
 });
 
 export default axiosInstance;
-
-// import axios from "axios";
-
-// const axiosClient = axios.create({
-//   baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
-// });
-
-// axiosClient.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("ACCESS_TOKEN");
-//   config.headers.Authorization = `Bearer ${token}`;
-
-//   return config;
-// });
-
-// axiosClient.interceptors.response.use(
-//   (response) => {
-//     return response;
-//   },
-//   (error) => {
-//     const { response } = error;
-
-//     if (response.status === 401) {
-//       localStorage.removeItem("ACCESS_TOKEN");
-//     }
-
-//     throw error;
-//   }
-// );
-
-// export default axiosClient;
